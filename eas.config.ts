5ae62b18663fe2a0b8e45c8a99ef7f07e1b32f43
Kirill Zaidulin <zaidulinkirill@gmail.com>
2024-08-22 23:07:03 +0300

    stash

--- eas.config.ts
+++ eas.config.ts
@@ -18,20 +18,10 @@ export default (): EasConfig => ({
     },
 
     development: {
-      env: {
-        APP_NAME: "Test",
-        APP_BUNDLE_ID: "appstrain.pl.cleaner-4",
-        APP_VERSION: "1.0",
-      },
       ios: {},
     },
 
     production: {
-      env: {
-        APP_NAME: "Test",
-        APP_BUNDLE_ID: "appstrain.pl.cleaner-4",
-        APP_VERSION: "1.0",
-      },
       ios: {},
     },
   },
